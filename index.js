34e007680ee49fec4ca1ecfa793d7ec0af70c3d3
audio <57118993+audioo@users.noreply.github.com>
2020-10-06 11:45:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-const Discord = require('discord.js')
+const { Client, Collection} = require('discord.js');
 const fs = require('fs')
 const { RichEmbed } = require('discord.js')
 const config = require('./util/config.json')
 for(let i = 0; i < config.tokens.length; i++) {
 
-console.log(`\x1b[47m`, `\x1b[30m`);
-
-let client = new Discord.Client()
-client.commands = new Discord.Collection()
-// Command Handler
-fs.readdirSync("./modules").forEach(folders => {
-    fs.readdirSync(`./modules/${folders}`).forEach(i => { 
-     if (!i.endsWith(".js")) return;
-     let commandFile = i.split(".")[0].trim()
-     client.commands.set(commandFile, require(`./modules/${folders}/${commandFile}.js`))
+  const client = new Client();
+  client.commands = new Collection()
+  // Command Handler
+  fs.readdirSync("./modules").forEach(folders => {
+    fs.readdirSync(`./modules/${folders}`).forEach(i => {
+      if (!i.endsWith(".js")) return;
+      let commandFile = i.split(".")[0].trim()
+      client.commands.set(commandFile, require(`./modules/${folders}/${commandFile}.js`))
     })
 
-})
-console.log(`Loaded ${client.commands.size} commands.`) 
+  })
+  console.log(`[+][SERVER] -> Loaded ${client.commands.size} commands.`)
 
-const evtFiles = fs.readdirSync("./events/")
-evtFiles.forEach(file => {
-  const eventName = file.split(".")[0];
-  const event = require(`./events/${file}`);
-  client.on(eventName, event.bind(null, client));
-});
+  const evtFiles = fs.readdirSync("./events/")
+  evtFiles.forEach(file => {
+    const eventName = file.split(".")[0];
+    const event = require(`./events/${file}`);
+    client.on(eventName, event.bind(null, client));
+  });
 
-console.log(`Loaded ${evtFiles.length} events.`)
+  console.log(`[+][SERVER] -> Loaded ${evtFiles.length} events.`)
 
-
-client.login(config.tokens[i])
+  client.login(config.tokens[i])
 }
