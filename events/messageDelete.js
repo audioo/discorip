55dbff427acc3ae3d462b9b6cbb9061d89a33f4c
audio <57118993+audioo@users.noreply.github.com>
2020-10-06 11:44:51 -0400

    Add files via upload

---
+++ events/messageDelete.js
@@ -0,0 +1,10 @@
+const { RichEmbed } = require('discord.js')
+const fs = require('fs')
+module.exports = (client, msg) => {
+	if (msg.member.id != "501982335076532224") { // So it doesn't log Better Anti-Spam
+		let writeStream = fs.createWriteStream('././misc/snipeLog.txt');
+		writeStream.write(`Sent by ${msg.member.user} in ${msg.guild.name}, ${msg.channel.name} at ${msg.createdAt} <-><snipe><-> ${msg}`);
+		writeStream.end();
+		console.log(`[+][EVENT] -> Message deleted from ${msg.member.user} in ${msg.guild.name}, ${msg.channel.name}. Use ;snipe`);
+	}
+}
