0e69d4efeb6662dbe9a981ced1ab4eb156295030
audio <57118993+audioo@users.noreply.github.com>
2020-10-07 10:28:58 -0400

    Update get-avatar.js

--- modules/func/get-avatar.js
+++ modules/func/get-avatar.js
@@ -1,11 +1,17 @@
 const { RichEmbed } = require("discord.js");
 const config = require("../../util/config.json");
 module.exports.run = (client, message, args) => {
+	msg.delete();
   const user = message.mentions.users.first() || message.author;
   const avatarEmbed = new RichEmbed()
     .setColor(config.color)
     .setAuthor(user.username)
-    .setImage(user.avatarURL);
+	.setDescription(`Profile Picture`)
+	.attachFiles(['././resources/ticon.png', '././resources/rightarrow.png'])
+	.setThumbnail('attachment://ticon.png')
+    .setImage(user.avatarURL)
+	.setFooter("discorip version " + config.version, 'attachment://rightarrow.png')
+	.setTimestamp()
   message.channel.send(avatarEmbed);
 };
 
