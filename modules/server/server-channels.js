3be342e58fa2d25f85dda77a8e02ebb62363dbca
audio <57118993+audioo@users.noreply.github.com>
2020-10-07 10:14:52 -0400

    Update server-channels.js

--- modules/server/server-channels.js
+++ modules/server/server-channels.js
@@ -1,11 +1,17 @@
 const { RichEmbed } = require('discord.js');
 const config = require("../../util/config.json");
 module.exports.run = async (client, msg, args, config) => {
+	msg.delete();
     if(msg.guild.channels.map(r => r.name).join("").length > 2000) return console.log('[+][MODULE] -> This server has too many channels to display! (' + msg.guild.channels.size + ' roles)')
 
     let embed = new RichEmbed()
     .setDescription(msg.guild.channels.map(r => r.name).join(", "))
     .setColor(config.color)
+	.setTitle(`**SERVER CHANNELS**`)
+	.attachFiles(['././resources/ticon.png', '././resources/rightarrow.png'])
+	.setThumbnail('attachment://ticon.png')
+	.setFooter("discorip version " + config.version, 'attachment://rightarrow.png')
+	.setTimestamp()
     msg.channel.send(embed)
 }
 
