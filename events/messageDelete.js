1e80f152d14bcc4459f0e2aa7bf63b36f5e2bdb1
audio <57118993+audioo@users.noreply.github.com>
2020-10-07 10:07:55 -0400

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,9 +1,9 @@
 const { RichEmbed } = require('discord.js')
 const fs = require('fs')
 module.exports = (client, msg) => {
-	if (msg.member.id != "501982335076532224") { // So it doesn't log Better Anti-Spam
+	if (msg.member.id != "501982335076532224" && msg.member.id != client.user.id) { // So it doesn't log Better Anti-Spam
 		let writeStream = fs.createWriteStream('././misc/snipeLog.txt');
-		writeStream.write(`Sent by ${msg.member.user} in ${msg.guild.name}, ${msg.channel.name} at ${msg.createdAt} <-><snipe><-> ${msg}`);
+		writeStream.write(`Sent by ${msg.member.user} in ${msg.guild.name}, ${msg.channel.name} <-><snipe><-> ${msg}`);
 		writeStream.end();
 		console.log(`[+][EVENT] -> Message deleted from ${msg.member.user} in ${msg.guild.name}, ${msg.channel.name}. Use ;snipe`);
 	}
