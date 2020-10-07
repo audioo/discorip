ff7fcae5d73c7e534900d17b8f4606e6e5e83cb5
audio <57118993+audioo@users.noreply.github.com>
2020-10-07 10:12:18 -0400

    Update about.js

--- modules/info/about.js
+++ modules/info/about.js
@@ -1,17 +1,18 @@
 const { RichEmbed } = require("discord.js");
 const config = require("../../util/config.json");
 module.exports.run = (client, msg, args, config) => {
+	msg.delete();
   const embed = new RichEmbed();
     embed.setTitle(`**ABOUT DISCORIP**`);
-	embed.attachFiles(['././resources/icon.png', '././resources/rightarrow.png']);
-	embed.setThumbnail('attachment://icon.png');
+	embed.attachFiles(['././resources/ticon.png', '././resources/rightarrow.png']);
+	embed.setThumbnail('attachment://ticon.png');
     embed.setDescription(`Selfbot created using NodeJS, with a purely utilitarian take on selfbots.`);
 	embed.addField(`Features`, `- No Nonsense\n- Easy Setup\n- Raid Tools\n- Lookups (email, ipv4, phone, etc...)\n- Custom Status\n- Hidden Server Information\n- User Information\n- Bot Farming\n- DM Tools\n- More to come!`, false);
 	embed.addField(`Repository`, `https://github.com/audioo/discorip`, true);
 	embed.addField(`Server`, `https://discord.gg/jnENWWp`, true);
 	embed.addField(`Contact`, `audio#0001`, true);
     embed.setColor(config.color);
-	embed.setFooter("discorip version 2.1", 'attachment://rightarrow.png');
+	embed.setFooter("discorip version " + config.version, 'attachment://rightarrow.png');
 	embed.setTimestamp();
 
   msg.channel.send(embed);
