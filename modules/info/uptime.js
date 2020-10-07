4f1ace95f38f3dd9b3e3995a79680a0146b46dec
audio <57118993+audioo@users.noreply.github.com>
2020-10-07 10:12:45 -0400

    Update uptime.js

--- modules/info/uptime.js
+++ modules/info/uptime.js
@@ -2,6 +2,7 @@ const countdown = require("countdown");
 const { RichEmbed } = require("discord.js");
 const config = require("../../util/config.json");
 module.exports.run = (client, msg, args, config) => {
+	msg.delete();
   let time = countdown(
     client.uptime,
     0,
@@ -10,11 +11,14 @@ module.exports.run = (client, msg, args, config) => {
 
   let embed = new RichEmbed()
     .setTitle(`**UPTIME**`)
+	.attachFiles(['././resources/ticon.png', '././resources/rightarrow.png'])
     .setDescription(
       `Days: **${time.days}**\nHours: **${time.hours}**\nMinutes: **${time.minutes}**\nSeconds: **${time.seconds}**`
     )
-    .setColor(config.color);
-
+    .setColor(config.color)
+	.setThumbnail('attachment://ticon.png')
+	.setFooter("discorip version " + config.version, 'attachment://rightarrow.png')
+	.setTimestamp()
   msg.channel.send(embed);
 };
 
