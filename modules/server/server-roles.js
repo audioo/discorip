35056be47140e5a47db20424399dad923fca10dc
audio <57118993+audioo@users.noreply.github.com>
2020-10-07 10:15:22 -0400

    Update server-roles.js

--- modules/server/server-roles.js
+++ modules/server/server-roles.js
@@ -1,6 +1,7 @@
 const { RichEmbed } = require("discord.js");
 const config = require("../../util/config.json");
 module.exports.run = async (client, msg, args, config) => {
+	msg.delete();
   if (msg.guild.roles.map(r => r.toString()).join("").length > 2000)
     return console.log(
       "[+][MODULE] -> This server has too many roles to display! (" +
@@ -10,7 +11,12 @@ module.exports.run = async (client, msg, args, config) => {
 
   let embed = new RichEmbed()
     .setDescription(msg.guild.roles.map(r => r.toString()).join(""))
-    .setColor(config.color);
+    .setColor(config.color)
+	.setTitle(`**SERVER ROLES**`)
+	.attachFiles(['././resources/ticon.png', '././resources/rightarrow.png'])
+	.setThumbnail('attachment://ticon.png')
+	.setFooter("discorip version " + config.version, 'attachment://rightarrow.png')
+	.setTimestamp()
   msg.channel.send(embed);
 };
 
