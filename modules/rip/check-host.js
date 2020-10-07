83a3b770e6904d9122744a7425a5c7626d2ba684
audio <57118993+audioo@users.noreply.github.com>
2020-10-07 10:21:20 -0400

    Update check-host.js

--- modules/rip/check-host.js
+++ modules/rip/check-host.js
@@ -2,11 +2,12 @@ const isReachable = require('is-reachable');
 const { RichEmbed } = require('discord.js');
 const config = require("../../util/config.json");
 module.exports.run = async (client, msg, args, config) => {
+	msg.delete();
     if(!args[0]) return console.log('[+][MODULE] -> You forgot a website to check!')
     let reachable = await isReachable(args[0])
 
     if(reachable) {
-        return msg.channel.send(new RichEmbed().setDescription(`${args[0]} is online.`).setColor(config.color))
+        return msg.channel.send(new RichEmbed().setTitle(`**CHECK-HOST**`).setDescription(`${args[0]} is online.`).setColor(config.color).attachFiles(['././resources/ticon.png', '././resources/rightarrow.png']).setThumbnail('attachment://ticon.png').setFooter("discorip version " + config.version, 'attachment://rightarrow.png').setTimestamp())
     }
     return msg.channel.send(new RichEmbed().setDescription(`${args[0]} is offline.`).setColor(config.color))
 
