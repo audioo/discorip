345f1e13eff2306d950d64da77d11737b925af88
audio <57118993+audioo@users.noreply.github.com>
2020-10-07 10:27:38 -0400

    Update send-embed.js

--- modules/func/send-embed.js
+++ modules/func/send-embed.js
@@ -1,9 +1,10 @@
 const { RichEmbed } = require('discord.js')
 const config = require("../../util/config.json");
 module.exports.run = async (client, msg, args, config) => {
+	msg.delete();
 
     if (args.join(" ").length > 2000) console.log('[+][MODULE] -> Message may not exceed 2000 characters.')
-    msg.channel.send(new RichEmbed().setDescription(args.join(" ")).setColor(config.color))
+    msg.channel.send(new RichEmbed().setTitle(`**EMBED**`).setDescription(args.join(" ")).setColor(config.color).attachFiles(['././resources/ticon.png', '././resources/rightarrow.png']).setThumbnail('attachment://ticon.png').setFooter("discorip version " + config.version, 'attachment://rightarrow.png').setTimestamp())
 
 }
 
